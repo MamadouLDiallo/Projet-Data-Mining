51307d59c03ba8c72a690889ef49fe6d87f2d829
MamadouLDiallo <mamadoulamaranadiallomld1@gmail.com>
2026-06-13 13:31:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -532,7 +532,7 @@ def modeling_and_predictions():
 
 def Summary():
     st.header("Analyse des Ventes - Résumé Exécutif")
-    st.write("Voici Votre Analyse des Ventes", divider='star')
+    st.header("Voici Votre Analyse des Ventes", divider="star")
 
     if 'df' in st.session_state and st.session_state['df'] is not None:
         df_selection = st.session_state['df'].copy() # Travailler sur une copie
