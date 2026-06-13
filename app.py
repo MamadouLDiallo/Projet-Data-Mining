10ab34e6af41dc98beffea59f32e7b39b47652c2
MamadouLDiallo <mamadoulamaranadiallomld1@gmail.com>
2026-06-13 13:35:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -538,7 +538,7 @@ def Summary():
 # Ou avec le style "star"
 st.header("Voici Votre Analyse des Ventes", divider="star")
 
-    if 'df' in st.session_state and st.session_state['df'] is not None:
+if 'df' in st.session_state and st.session_state['df'] is not None:
         df_selection = st.session_state['df'].copy() # Travailler sur une copie
 
         # Assurez-vous que 'InvoiceDate' est bien un type datetime
